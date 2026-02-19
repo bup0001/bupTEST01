36e1baccbf65035ade5319815cb44ab7702fbc74
bup0001 <bupits6@gmail.com>
2026-02-19 15:50:27 +0100

    Update ControlPanel.cs

--- ControlPanel.cs
+++ ControlPanel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Drawing;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using MultiMonitorPlatform.Automation;
 using MultiMonitorPlatform.Core;
@@ -506,6 +507,3 @@ namespace MultiMonitorPlatform.UI
         }
     }
 }
-
-// Task import
-using System.Threading.Tasks;
